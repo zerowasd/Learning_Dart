90a6026ad59911ac1a758c14d7550599975bbbfe
zerowasd <neerajkumaryadav01102003@gmail.com>
2025-12-26 23:19:23 +0530

    collections

--- 8eighth.dart
+++ 8eighth.dart
@@ -1,4 +1,4 @@
-///collections 
+///collections - List[order], Set{unique}, Map{key-value}
 
 void main(){
     //list - ordered
@@ -32,5 +32,71 @@ void main(){
     List<String>? names3;//list can be null
     print("$names2 $names3");
 
+    ///sets - unique values only
+    Set<int> numbers = {1,2,3,3,4,5,5};//shows warning (intentional)
+    print("sets $numbers");//removes duplicates automatically , do not guarantee order
+
+    //set operation
+    numbers.add(6);
+    print("add $numbers");
+
+    numbers.remove(3);
+    print("add $numbers");
+    print(numbers.contains(6));//usage -> when you want uniqueness, order does not matter, fast look up
+
+    ///maps - key-value pairs
+    Map<String, int> scores= {
+        "Math" : 90,
+        "Science" : 84
+    };
+
+    ///infered map - it's short and simple
+    var inferedscores = {
+        "Math": 90,
+        "Science": 85,
+    };
     
+    print(scores);
+    print(inferedscores);
+
+    //accessing map values
+    print(scores["Math"]);//if key does not exist returns null
+
+    //map - add and update
+    scores["English"] = 80;
+    print(scores);
+    inferedscores["Math"] = 80;
+    print(inferedscores);
+
+    //remove
+    scores.remove("Science");
+    print(scores);
+
+    //properties
+    print(scores.keys);
+    print(scores.values);
+    print(scores.containsKey("Math"));
+
+    /**
+     * IMP
+     * Map<String, int>? map1; -> map can be null
+     * Map<String, int?> map2; -> map-value can be null
+     * 
+     */
+
+    ///iterating
+    //list and set
+
+    for(var subject in names){
+        print("iteration $subject");
+    }
+
+    //map
+    print(scores);
+    for (var entry in scores.entries/**this is getter not a method */){
+        print("${entry.key} : ${entry.value}");
+    }
+
+    //var set = <int>{}; BY DEFAULT {} BECOMES A MAP SO WHEN MAKING INFERED SET DO IT LIKE THIS
+
 }
