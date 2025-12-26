baea21daeb955af30f07abff00a360a951a55ff7
zerowasd <neerajkumaryadav01102003@gmail.com>
2025-12-26 22:29:44 +0530

    collections - Lists

--- 8eighth.dart
+++ 8eighth.dart
@@ -1,2 +1,36 @@
-///collections
+///collections 
 
+void main(){
+    //list - ordered
+    List<String> names = ['zerowasd','two','three'];
+
+    var inferedname = ['zerowasd','two','three'];
+
+    //accessing
+    print(names[0]);
+    print(inferedname[0]);
+
+    //modifying
+    print("original $names");
+    names.add('four');
+
+    print("added $names");
+    names.remove('three');
+
+    print("removed $names");
+
+    var nameschanged = names[0] = "zero";
+    print(nameschanged);
+    
+    //list properties and methods
+    print("length $names.length");
+    print(names.isEmpty);
+    print(names.contains("zero"));
+
+    //list and null safety
+    List<String?/** IMP */> names2 = ["zerwoasd","two",null];// defined the elements can be null
+    List<String>? names3;//list can be null
+    print("$names2 $names3");
+
+    
+}
